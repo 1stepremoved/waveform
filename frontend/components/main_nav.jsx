a5de07084444b9968675893a7689481b65d89713
Hamilton Sands <hamiltonbsands@gmail.com>
2018-01-03 16:17:22 -0500

    worked on css for main-nav

--- frontend/components/main_nav.jsx
+++ frontend/components/main_nav.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
  class MainNav extends React.Component{
    render() {
      return (
        <section className="main-nav">
-         <nav className="main-nav-links">
-           <img src={window.staticImages.logo} className="main-logo"></img>
-           <NavLink to="/stream" className="main-nav-button" activeStyle={{ backgroundColor: 'gray' }}>Home</NavLink>
-           <NavLink to="/collection" className="main-nav-button" activeStyle={{ backgroundColor: 'gray' }}>Collection</NavLink>
-         </nav>
+         <ul className="main-nav-links">
+           <li className="main-nav-link">
+             <Link to="/" className="main-nav-logo-button">
+               <img src={window.staticImages.logo} className="main-logo"></img>
+             </Link>
+           </li>
+           <li className="main-nav-link">
+             <NavLink to="/stream" className="main-nav-button">Home</NavLink>
+           </li>
+           <li className="main-nav-link">
+             <NavLink to="/collection" className="main-nav-button">Collection</NavLink>
+           </li>
+           <li className="main-nav-link">
+             <NavLink to="/login" className="main-nav-login-button">Sign In</NavLink>
+           </li>
+           <li className="main-nav-link">
+             <NavLink to="/signup" className="main-nav-signup-button">Create Account</NavLink>
+           </li>
+         </ul>
        </section>
      );
    }
