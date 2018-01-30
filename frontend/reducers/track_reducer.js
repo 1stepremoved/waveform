90e70561c80270ef289399ff7435e462187c7788
Hamilton Sands <hamiltonbsands@gmail.com>
2018-01-30 10:19:27 -0500

    commented out fetch

--- frontend/reducers/track_reducer.js
+++ frontend/reducers/track_reducer.js
@@ -81,34 +81,34 @@ const trackReducer = (state = {}, action) => {
       if (!track.audio) {
         track.audio = new Audio();
         track.audio.src = track.audioUrl;
-        fetch(track.audioUrl, {})
-        .then((res) => {
-          let reader = res.body.getReader();
-          return new ReadableStream({
-            start(controller) {
-              return pump();
-              function pump() {
-                return reader.read().then(({ done, value }) => {
-                  // When no more data needs to be consumed, close the stream
-                  if (done) {
-                    controller.close();
-                    return;
-                  }
-                  // Enqueue the next data chunk into our target stream
-                  controller.enqueue(value);
-                  return pump();
-                });
-              }
-            }
-          });
-        })
-        .then(stream => new Response(stream))
-        .then(response => response.blob())
-        .then(blob => URL.createObjectURL(blob))
-        .catch(err => console.error(err))
-        .then(dataURL => {
-          track.audio.src = dataURL;
-        });
+        // fetch(track.audioUrl, {})
+        // .then((res) => {
+        //   let reader = res.body.getReader();
+        //   return new ReadableStream({
+        //     start(controller) {
+        //       return pump();
+        //       function pump() {
+        //         return reader.read().then(({ done, value }) => {
+        //           // When no more data needs to be consumed, close the stream
+        //           if (done) {
+        //             controller.close();
+        //             return;
+        //           }
+        //           // Enqueue the next data chunk into our target stream
+        //           controller.enqueue(value);
+        //           return pump();
+        //         });
+        //       }
+        //     }
+        //   });
+        // })
+        // .then(stream => new Response(stream))
+        // .then(response => response.blob())
+        // .then(blob => URL.createObjectURL(blob))
+        // .catch(err => console.error(err))
+        // .then(dataURL => {
+        //   track.audio.src = dataURL;
+        // });
         track.audio.load();
       }
       return newState;
